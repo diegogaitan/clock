03c2ede4207b6b52412c90257d4cc17765ca1ae7
Diego Gaitán <bairesdev_dgaitan@iseatz.com>
2020-11-05 17:13:37 -0500

    Repo set up

---
+++ lib/clock.ex
@@ -0,0 +1,18 @@
+defmodule Clock do
+  @moduledoc """
+  Documentation for `Clock`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clock.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
