67100fe46f08ec76ad2c4ea77456cc466b8c4e81
Diego Gaitán <bairesdev_dgaitan@iseatz.com>
2020-11-09 19:28:50 -0500

    Improved core to be able to add time

--- lib/clock/core.ex
+++ lib/clock/core.ex
@@ -1,12 +1,24 @@
 defmodule Clock.Core do
   use Timex
 
-  def current_time do
-    {:ok, now} =
-      "America/Bogota"
-      |> Timex.now()
-      |> Timex.format("{h12}:{m} {AM}")
-
+  def format_time(time) do
+    {:ok, now} = Timex.format(time, "{h12}:{m} {AM}")
     now
+  end
+
+  def current_time(time_zone \\ "America/Bogota") do
+    time_zone |> Timex.now()
+  end
+
+  def duration_minutes(minutes) when is_integer(minutes) do
+    Timex.Duration.from_minutes(minutes)
+  end
+
+  def add(%DateTime{} = time, %Timex.Duration{} = duration) do
+    Timex.add(time, duration)
+  end
+
+  def diff_minutes(%DateTime{} = time1, %DateTime{} = time2) do
+    Timex.diff(time1, time2, :minute)
   end
 end
--- test/clock/core_test.exs
+++ test/clock/core_test.exs
@@ -1,9 +1,27 @@
 defmodule Clock.CoreTest do
   use ExUnit.Case
+  alias Clock.Core
 
   test "current_time" do
+    assert %DateTime{} = Core.current_time()
+  end
+
+  test "format_time" do
     # Matches this time format: 7:25 PM"
-    assert Clock.Core.current_time()
+    assert Core.current_time()
+           |> Core.format_time()
            |> String.match?(~r/^(1[0-2]|0?[1-9]):([0-5]?[0-9])(\s[AP]M)$/)
+  end
+
+  test "duration_minutes" do
+    assert %Timex.Duration{} = Core.duration_minutes(5)
+  end
+
+  test "add" do
+    time = Core.current_time()
+    duration = Core.duration_minutes(5)
+    new_time = Core.add(time, duration)
+
+    assert 5 = Core.diff_minutes(new_time, time)
   end
 end
