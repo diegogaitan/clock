30cd597bbde4b530176fdb18416f10bd7dcc8d83
Diego Gaitán <bairesdev_dgaitan@iseatz.com>
2020-11-10 16:11:40 -0500

    mix format

--- test/clock_test.exs
+++ test/clock_test.exs
@@ -5,18 +5,19 @@ defmodule ClockTest do
   @tick_seconds 1
 
   setup do
-    clock = start_supervised!(%{
-      id: Clock,
-      start: {Clock, :start_link, [[tick_seconds: @tick_seconds]]}
-    })
+    clock =
+      start_supervised!(%{
+        id: Clock,
+        start: {Clock, :start_link, [[tick_seconds: @tick_seconds]]}
+      })
 
     %{:clock => clock}
   end
 
   test "time", %{clock: clock} do
     assert clock
-    |> Clock.time()
-    |> String.match?(Core.format_time_regex())
+           |> Clock.time()
+           |> String.match?(Core.format_time_regex())
   end
 
   test "tick", %{clock: clock} do
@@ -27,5 +28,4 @@ defmodule ClockTest do
 
     assert Core.diff_seconds(end_time, start_time) >= @tick_seconds
   end
-
 end
